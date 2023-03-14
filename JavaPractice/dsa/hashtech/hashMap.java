c99d581a81d2cccdae3efef5517ad94ae325ae0d
averma2514 <79777996+averma2514@users.noreply.github.com>
2023-03-14 10:08:35 +0000

    uncomplete hasmap.java upload

---
+++ JavaPractice/dsa/hashtech/hashMap.java
@@ -0,0 +1,10 @@
+package dsa.hashmap;
+
+class country{
+    private String name;
+    private int population;
+    country(String name,int population){
+        this.name=name;
+        this.population=population;
+    }
+}
