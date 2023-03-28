ee83a77bc5bb41e772decfb7c05f799714a7ca79
averma2514 <79777996+averma2514@users.noreply.github.com>
2023-03-28 05:49:34 +0000

    update tree.java

--- JavaPractice/dsa/tree/tree.java
+++ JavaPractice/dsa/tree/tree.java
@@ -1,42 +1,41 @@
 package dsa.tree;
 
-class Tree{
+class Tree {
     Node root;
 
-    class Node{
+    class Node {
         int data;
         Node left;
         Node right;
 
-        Node(int data){
+        Node(int data) {
             this.data = data;
             left = null;
             right = null;
         }
     }
 
-       
-    public void insert(int data){
+    public void insert(int data) {
         Node newnode = new Node(data);
-        
-        if(root == null){
+
+        if (root == null) {
             root = newnode;
-        }else{
+        } else {
             Node current = root;
 
-            while(true){
-                if(data<current.data){
-                    if(current.left == null){
+            while (true) {
+                if (data < current.data) {
+                    if (current.left == null) {
                         current.left = newnode;
                         break;
-                    }else{
+                    } else {
                         current = current.left;
                     }
-                }else{
-                    if(current.right == null){
+                } else {
+                    if (current.right == null) {
                         current.right = newnode;
                         break;
-                    }else{
+                    } else {
                         current = current.right;
                     }
                 }
@@ -44,64 +43,95 @@ class Tree{
         }
     }
 
-    void inOrder(Node temp){
-       if(temp!=null){
-         inOrder(temp.left);
-         System.out.println(temp.data + " ");            
-         inOrder(temp.right);
-      }
+    void inOrder(Node temp) {
+        if (temp != null) {
+            inOrder(temp.left);
+            System.out.println(temp.data + " ");
+            inOrder(temp.right);
+        }
     }
 
-    void preorder(Node temp){
-       if(temp!=null){
-        System.out.print(temp.data + " "); 
-         preorder(temp.left);
-                    
-         preorder(temp.right);
-      }
+    void preorder(Node temp) {
+        if (temp != null) {
+            System.out.print(temp.data + " ");
+            preorder(temp.left);
+
+            preorder(temp.right);
+        }
     }
 
-    void inorder(Node temp){
-       if(temp!=null){
-         inorder(temp.left);
-         System.out.print(temp.data + " ");            
-         inorder(temp.right);
-      }
+    void inorder(Node temp) {
+        if (temp != null) {
+            inorder(temp.left);
+            System.out.print(temp.data + " ");
+            inorder(temp.right);
+        }
     }
 
-    void postorder(Node temp){
-       if(temp!=null){
-         postorder(temp.left);
-         postorder(temp.right);
-         System.out.print(temp.data + " "); 
-      }
+    void postorder(Node temp) {
+        if (temp != null) {
+            postorder(temp.left);
+            postorder(temp.right);
+            System.out.print(temp.data + " ");
+        }
     }
 
+    void delete(int data) {
+        Node parent = root;
+        Node temp;
+        if (parent == null) {
+            System.out.println("Tree is empty");
+            return;
+        } else {
+            while (data != parent.data) {
+                if (data < parent.data) {
+                    parent = parent.left;
+                    if (parent == null) {
+                        System.out.println("Element not present");
+                        return;
+                    }
+                } else {
+                    parent = parent.right;
+                    if (parent == null) {
+                        System.out.println("Element not present");
+                        return;
+                    }
+                }
+            }
+            
 
+        }
+    }
 
     public static void main(String[] args) {
         Tree tree = new Tree();
-        //element entring in tree
+        // element entring in tree
         tree.insert(5);
         tree.insert(3);
         tree.insert(1);
         tree.insert(8);
 
-        //printng element.
+        // printng element.
         System.out.print("Element in tree = > ");
         tree.preorder(tree.root);
         System.out.println();
 
-        //printng element in incrising order.
+        // printng element in incrising order.
         System.out.print("Element in tree in incrising order = > ");
         tree.inorder(tree.root);
         System.out.println();
-        
-        //printing element in decrising order.
+
+        // printing element in decrising order.
         System.out.print("Element in tree in decrising order = > ");
         tree.postorder(tree.root);
         System.out.println();
+
+        // delete element from tree.
+        tree.delete(3);
+        System.out.print("Element in tree after delete = > ");
+        tree.preorder(tree.root);
+        System.out.println();
+
     }
-    
 
 }
