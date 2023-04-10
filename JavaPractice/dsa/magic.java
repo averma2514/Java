4a25aeba263addf44c47833b2129c445bcd242f3
averma2514 <79777996+averma2514@users.noreply.github.com>
2023-04-10 00:03:06 -0700

    Update magic.java
    solve error

--- JavaPractice/dsa/magic.java
+++ JavaPractice/dsa/magic.java
@@ -4,7 +4,7 @@ import java.nio.file.FileAlreadyExistsException;
 class magic{
     public static void main(String[] args) {
         String path = "..\\";
-        for(int i=1;i>0i++){
+        for(int i=1;i>0;i++){
             File file = new File(i+".text");
             try{
                 file.createNewFile();
